303b82bbc0300e53929f300cf9bcfd51858cc886
Jose Ruzafa <jose.ruzafa@gmail.com>
2024-09-14 21:58:57 +0200

    feat: normalize error to user and log

--- src/App/TweetBundle/Controller/TweetConverterController.php
+++ src/App/TweetBundle/Controller/TweetConverterController.php
@@ -2,6 +2,7 @@
 
 namespace App\TweetBundle\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,30 +12,27 @@ use Xceed\Tweet\Infrastructure\TweetTextTransformer;
 
 readonly final class TweetConverterController
 {
-    private GetTweetsByUserName $useCase;
-    private TweetTextTransformer $transformer;
-
-    public function __construct(GetTweetsByUserName $useCase, TweetTextTransformer $transformer)
-    {
-        $this->useCase = $useCase;
-        $this->transformer = $transformer;
-    }
+    public function __construct(
+        private GetTweetsByUserName $useCase,
+        private TweetTextTransformer $transformer,
+        private LoggerInterface $logger
+    )
+    {}
 
     public function __invoke(Request $request, string $userName): JsonResponse
     {
-        // todo: add validation from parameters
-        $limit = $request->get('limit');
+        $limit = $request->get('limit', 0);
 
         try {
-            $response = $this->useCase->execute(
-                new GetTweetsByUserNameRequest($userName, (int) $limit)
-            );
+            $response = $this->useCase->execute(new GetTweetsByUserNameRequest($userName, (int) $limit));
 
             return new JsonResponse($this->transformer->transform($response->tweets()));
         } catch (\InvalidArgumentException $e) {
             return new JsonResponse(['error' => $e->getMessage()], Response::HTTP_BAD_REQUEST);
         } catch (\Exception $e) {
-            return new JsonResponse(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+            $this->logger->error($e->getMessage());
+
+            return new JsonResponse(['error' => 'Internal server error'], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 }
